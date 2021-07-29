4e3800a946cd5032d5c2f633fb5a17b7c3b5ef70
mineteeraa <patteera_mine@hotmail.com>
2021-07-29 16:10:40 +0700

    Add flash-message in register and edit

--- src/views/event/Details.vue
+++ src/views/event/Details.vue
@@ -1,10 +1,29 @@
 <template>
+  <div id="flashMessage_Edit" v-if="GStore.flashMessage_Edit">
+    {{ GStore.flashMessage_Edit }}
+  </div>
   <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
   <p>{{ event.description }}</p>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore']
 }
 </script>
+
+<style>
+@keyframes greenfade {
+  from {
+    background: #4bd84b;
+  }
+  to {
+    background: transparent;
+  }
+}
+#flashMessage_Edit {
+  animation-name: greenfade;
+  animation-duration: 3s;
+}
+</style>
--- src/views/event/Edit.vue
+++ src/views/event/Edit.vue
@@ -1,9 +1,30 @@
 <template>
   <p>Edit the event here</p>
+  <button @click="edit">Edit Me</button>
 </template>
 
 <script>
 export default {
-  props: ['event']
+  props: ['event'],
+  inject: ['GStore'],
+  methods: {
+    edit() {
+      //Assuming successful API call to edit them
+      //Set a flash message to appear on the next page loaded which says
+      //'The data has been updated for ' + this.event.title
+
+      this.GStore.flashMessage_Edit =
+        'The data has been updated for ' + this.event.title
+      setTimeout(() => {
+        //After 3 seconds remove it
+        this.GStore.flashMessage_Edit = ''
+      }, 3000)
+
+      this.$router.push({
+        name: 'EventDetails',
+        params: { id: this.event.id }
+      })
+    }
+  }
 }
 </script>
